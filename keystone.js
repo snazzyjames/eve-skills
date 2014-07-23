4e9d1b52249d3e25bd10cc752c01a2bd8cde3bea
James <snazzyjames@gmail.com>
2014-07-22 19:18:33 -0500

    test

--- keystone.js
+++ keystone.js
@@ -4,7 +4,7 @@ require('dotenv').load();
 
 // Require keystone
 var keystone = require('keystone');
-
+//test
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
